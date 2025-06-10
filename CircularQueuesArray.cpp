43eb3b8f702867b68f4d0785ccdfd6804747f05b
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-06-10 11:14:41 +0700

    mengecek apakah antrian penuh atau tidak

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Queues{
+    private:
     int FRONT,REAR, max = 5;
     int queue_array[5];
 
@@ -17,4 +18,10 @@ class Queues{
             cin >> num;
             cout << endl;
 
-            
+            //cek apakah antrian penuh
+            if (( FRONT == 0 && REAR == max - 1 ) || ( FRONT == REAR + 1 )) {
+                cout << " \nQueue overflow\n";
+                return;
+            }
+
+        
