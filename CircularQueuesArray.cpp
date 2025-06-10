5ce479103d3a48456be4a0f5563acb53a6b6620c
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-06-10 11:24:09 +0700

    jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -53,4 +53,10 @@ class Queues{
                 REAR = 1;
             }
 
-           
+            //jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if ( FRONT == max -1 ) 
+                 FRONT = 0;
+            else    
+                FRONT = FRONT + 1;
+        }
+        
