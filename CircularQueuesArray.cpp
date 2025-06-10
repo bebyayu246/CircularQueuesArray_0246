9f35da37dd6e2f7c16a4a67827ad70465c64c59a
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-06-10 11:33:35 +0700

    cek apakah antrian kosong

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -63,4 +63,10 @@ class Queues{
     int FRONT_position = FRONT;
     int REAR_position = REAR;
 
-    
+    //cek apakah antrian kosong
+    if ( FRONT == -1 ) {
+        cout << " Queue is empty\n ";
+        return;
+    }
+
+   
