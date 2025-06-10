43c3da4c10c5d8ff7f8d560f2d77dcef9987d5c7
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-06-10 11:07:13 +0700

    tidak mengembalikan nilai apapun

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
-Class Queues {
-    int FRONT, REAR, max = 5;
+class Queues{
+    int FRONT,REAR, max = 5;
     int queue_array[5];
 
     public:
-        Queues() {
-            FRONT = -1;
-            REAR = -1;
-        }
-}
+    Queues(){
+        FRONT = -1;
+        REAR  = -1;
+    }
+
+        void insert() {
+            
