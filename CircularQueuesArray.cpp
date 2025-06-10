dd24772285c9a36fac41eac0707b2b13c0d39340
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-06-10 11:36:52 +0700

    iterasi dari awal array hingga REAR

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -88,4 +88,13 @@ class Queues{
 
         FRONT_position = 0;
 
-       
+        //iterasi dari awal array hingga REAR
+        while ( FRONT_position <= REAR_position ) {
+            cout << queue_array[FRONT_position] << " ";
+            FRONT_position++;
+        }
+        cout << endl;
+        }
+    }   
+};
+
